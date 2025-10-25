d1ee5bffb730b9a610d97808111075ffa01f5274
Климанов Данила Дмитриевич <danilawork2025@gmail.com>
2025-10-25 21:48:41 +0300

    new

--- app/static/js/body/accaunt.js
+++ app/static/js/body/accaunt.js
@@ -1,1 +1,32 @@
-const openButton = document.querySelector('.open-button');
+const openButton = document.querySelector('.user-avatar');
+const photoContainer = document.querySelector('.photo-container')
+const closeButton = document.querySelector('.close-button')
+
+const cancelButton = document.querySelector('.cancel-button')
+const deleateButton = document.querySelector('.deleate-Button')
+
+
+// Проверяем, есть ли изображение при загрузке страницы
+checkButtonState();
+
+// Открытие модального окна
+openButton.addEventListener('click', function() {
+    photoContainer.classList.add('show');
+    updateDeleteButtonState();
+});
+
+function closeModal() {
+    photoContainer.classList.remove('show');
+    resetForm();
+}
+
+closeButton.addEventListener('click', closeModal);
+cancelButton.addEventListener('click', closeModal);
+
+// Закрытие по клику на оверлей
+photoContainer.addEventListener('click', function(e) {
+    if (e.target === photoContainer) {
+        closeModal();
+    }
+});
+
